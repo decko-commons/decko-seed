7b3222c514eaa138606f4b04230af15bf829000d
Philipp Kühl <philipp.kuehl@gmail.com>
2019-03-01 19:46:20 +0100

    add pointer to seed data

--- test/seed.rb
+++ test/seed.rb
@@ -15,7 +15,6 @@ class SharedData
 
   CARDTYPE_COUNT = 35
 
-
   class << self
 
     def create_user name, args
@@ -145,6 +144,10 @@ class SharedData
 
       notification_cards
 
+      create "42", TEXT
+      create_pointer "items",
+                     content: ["Parry Hotter", "42", "Stam Broker", "First",
+                               "yeti skin+image", "*all+*script+*machine output"]
       # Card['*all+*style' ].ensure_machine_output
       # Card['*all+*script'].ensure_machine_output
       # (ie9 = Card[:script_html5shiv_printshiv]) && ie9.ensure_machine_output
@@ -291,4 +294,22 @@ class SharedData
 
     end
   end
+
+  TEXT = <<-TXT.strip_heredoc.freeze
+      Far out in the uncharted backwaters of the unfashionable end of
+      the western spiral arm of the Galaxy lies a small unregarded
+      yellow sun.
+      
+      Orbiting this at a distance of roughly ninety-two million miles
+      is an utterly insignificant little blue green planet whose ape-
+      descended life forms are so amazingly primitive that they still
+      think digital watches are a pretty neat idea.
+      
+      This planet has - or rather had - a problem, which was this: most
+      of the people living on it were unhappy for pretty much of the time.
+      Many solutions were suggested for this problem, but most of these
+      were largely concerned with the movements of small green pieces
+      of paper, which is odd because on the whole it wasn't the small
+      green pieces of paper that were unhappy.
+  TXT
 end
