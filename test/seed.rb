d6692d0f606c15ce85bfe134f8ceea25f47fda84
xithan <philipp.kuehl@gmail.com>
2019-07-10 20:04:45 +0200

    seed tick

--- test/seed.rb
+++ test/seed.rb
@@ -44,7 +44,7 @@ class SharedData
       # above still necessary?  try commenting out above and 'Sign up' below
       Card::Auth.current_id = Card::WagnBotID # need to reset after creating sign up, which changes current_id for extend phase
 
-      no_samples = %w[user sign_up set number list listed_by file image
+      no_samples = %w[user sign_up set number mirror_list mirrored_list file image
                       customized_bootswatch_skin]
       Card::Auth.createable_types.each do |type|
         next if no_samples.include? type.to_name.key
